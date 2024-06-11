165a6289cad6a61b65a078eaa74371ef8e4c1968
Muhamad Sihabudin <muhamadsihabudin@pop-os.localdomain>
2024-06-12 05:37:28 +0700

    fix bug app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,33 @@
+// import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/styles/fonts.module.css';
 import "../public/styles/globals.css";
-import { appWithTranslation } from 'next-i18next';
 import Navbar from './components/_navbar';
 import '../i18next.config.mjs';
 
-function MyApp({ Component, pageProps }) {
-return (
-  <>
-    <Navbar />
-    <Component {...pageProps} />
-  </>
-);
-}
+export default function App({ Component, pageProps }) {
+  return(
+    <>
+  <Navbar />
+  <Component {...pageProps} />
+    </>
+);}
 
-export default appWithTranslation(MyApp);
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../public/styles/fonts.module.css';
+// import "../public/styles/globals.css";
+// import { appWithTranslation } from 'next-i18next';
+// import Navbar from './components/_navbar';
+// import '../i18next.config.mjs';
+
+// function MyApp({ Component, pageProps }) {
+// return (
+//   <>
+//     <Navbar />
+//     <Component {...pageProps} />
+//   </>
+// );
+// }
+
+// export default appWithTranslation(MyApp);
