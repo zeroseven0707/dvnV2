95a154f9dbc1ba90fee2fbb1ddb7d2a531184824
Muhamad Sihabudin <muhamadsihabudin@pop-os.localdomain>
2024-06-13 05:20:51 +0700

    translate statis about dan lainya

--- pages/_app.js
+++ pages/_app.js
@@ -12,22 +12,3 @@ export default function App({ Component, pageProps }) {
   <Component {...pageProps} />
     </>
 );}
-
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import '../public/styles/fonts.module.css';
-// import "../public/styles/globals.css";
-// import { appWithTranslation } from 'next-i18next';
-// import Navbar from './components/_navbar';
-// import '../i18next.config.mjs';
-
-// function MyApp({ Component, pageProps }) {
-// return (
-//   <>
-//     <Navbar />
-//     <Component {...pageProps} />
-//   </>
-// );
-// }
-
-// export default appWithTranslation(MyApp);
