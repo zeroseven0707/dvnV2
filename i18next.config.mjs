7f758918796086e4241ef9b957d88f669a982705
Zero <msihabudin461@gmail@gmail.com>
2024-06-16 08:47:37 +0700

    revisi next.config

--- i18next.config.mjs
+++ i18next.config.mjs
@@ -1,8 +1,9 @@
-// i18next.config.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+
+const isServer = typeof window === 'undefined';
 
 i18n
   .use(Backend)
@@ -10,12 +11,12 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
-    debug: true,
+    debug: !isServer, // Enable debug only on the client-side
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // React already does escaping
     },
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      loadPath: isServer ? `${process.env.BASE_URL}/locales/{{lng}}/{{ns}}.json` : '/locales/{{lng}}/{{ns}}.json',
     },
   });
 
