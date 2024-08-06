a59d8b463a9a6f42aa9c256dee9c0565509fbed8
Zero <msihabudin461@gmail@gmail.com>
2024-08-06 20:28:43 +0700

    revisi reviews

--- pages/components/_reviews.js
+++ pages/components/_reviews.js
@@ -1,59 +1,42 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '@/styles/Reviews.module.css';
 import Image from "next/image";
 
 const Reviews = () => {
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+        const fetchReviews = async () => {
+            try {
+                const response = await fetch('https://prahwa.net/api/review', {
+                    method: 'GET',
+                    headers: {
+                        'api_key': 'TddGvkPqgaq1kv6LOckmTwXnz8uHE859qlnqW3d8'
+                    }
+                });
+                const data = await response.json();
+                setReviews(data.data);
+            } catch (error) {
+                console.error('Error fetching reviews:', error);
+            }
+        };
+        fetchReviews();
+    }, []);
     return (
         <div className={styles.reviewsContainerLayout}>
             <div className={styles.reviewsContainer}>
-                <div className={styles.review}>
-                    <div className={styles.images}>
-                        <Image src="/image/imagereviews.png" width={500} height={500} className={styles.imageReview} alt="" />
-                        <hr/>
+                {reviews.map(review => (
+                    <div key={review.id} className={styles.review}>
+                        <div className={styles.images}>
+                            <Image src="/image/imagereviews.png" width={500} height={500} className={styles.imageReview} alt="" />
+                            <hr/>
+                        </div>
+                        <b>{review.title}</b>
+                        <p dangerouslySetInnerHTML={{ __html: review.description }}></p>
+                        <p className={styles.tgl}>{(review.date)}</p>
                     </div>
-                    <b>Perfect!</b>
-                    <p>Noticed a difference in 7 days</p>
-                    <p className={styles.tgl}>Jan 21, 2024</p>
-                </div>
-                <div className={styles.review}>
-                <div className={styles.images}>
-                        <Image src="/image/imagereviews.png" width={500} height={500} className={styles.imageReview} alt=""/>
-                        <hr/>
-                    </div>
-                    <b>This is the best.</b>
-                    <p>Speechless at how powerful this is for my skin!</p>
-                    <p className={styles.tgl}>Jan 25, 2024</p>
-                </div>
-                <div className={styles.review}>
-                <div className={styles.images}>
-                        <Image src="/image/imagereviews.png" width={500} height={500} className={styles.imageReview} alt=""/>
-                        <hr/>
-                    </div>
-                    <b>Speechless</b>
-                    <p>I can&apos;t say anything than WONDERFUL product...</p>
-                    <p className={styles.tgl}>Feb 12, 2024</p>
-                </div>
-                <div className={styles.review}>
-                <div className={styles.images}>
-                        <Image src="/image/imagereviews.png" width={500} height={500} className={styles.imageReview} alt=""/>
-                        <hr/>
-                    </div>
-                    <b>WOW!</b>
-                    <p>Amazing stuff everyone should try this for sure!</p>
-                    <p className={styles.tgl}>Feb 15, 2024</p>
-                </div>
-                <div className={styles.review}>
-                <div className={styles.images}>
-                        <Image src="/image/imagereviews.png" width={500} height={500} className={styles.imageReview} alt=""/>
-                        <hr/>
-                    </div>
-                    <b>Make my skin soft</b>
-                    <p>Tried this for 5 days and already see the difference...</p>
-                    <p className={styles.tgl}>Feb 23, 2024</p>
-                </div>
+                ))}
             </div>
         </div>
     );
 };
-
 export default Reviews;
